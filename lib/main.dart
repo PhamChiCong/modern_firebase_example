4d35a6a23424806da4ef4514bfd27a3026fdae1e
sanni prasad <prasadsunny1@gmail.com>
2020-09-12 21:26:34 +0530

    fix typo

--- lib/main.dart
+++ lib/main.dart
@@ -47,7 +47,7 @@ class _HomePageState extends State<HomePage> {
   Future<UserCredential> signInWithGoogle() async {
     try {
       FirebaseAuth.instance.authStateChanges().listen(
-      (User user) {F
+      (User user) {
         if (user == null) {
           print('User is currently signed out!');
 
